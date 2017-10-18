6d618bcf49201f788256ae269f536c96f3955f0f
Vladimir V. Myagdeev <vvmspace@gmail.com>
2017-10-18 21:21:53 +0500

    CTO base

--- app/Cto.php
+++ app/Cto.php
@@ -7,9 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Cto extends Model
 {
     protected $api;
+    protected $buy_from;
+    protected $buy_to;
+    protected $sell_from;
+    protected $sell_to;
+
 
     function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
+        $this->api = new Api();
+        $this->buy_from = env('CTO_BUY_FROM');
+        $this->sell_from = env('CTO_SELL_FROM');
+        $this->buy_to = env('CTO_BUY_TO');
+        $this->sell_to = env('CTO_SELL_TO');
     }
 }
