bce7bb8e9578ba109ea9a841088af8e1c3b90ac0
vvmspace <concertbil@gmail.com>
2017-10-22 03:29:57 +0500

    Added Dash

--- app/Ticker.php
+++ app/Ticker.php
@@ -9,15 +9,7 @@ class Ticker extends Model
     static function Tick(){
         // $lastTick =  KV::get('last_tick');
         // KV::set('last_tick', time());
-        Cto::CancelRandomOrder();
-        sleep(10);
-        Cto::SellMicro();
-        sleep(10);
-        Cto::BuyMicro();
-        sleep(10);
-        Dazz::CancelRandomOrder();
-        sleep(10);
-        $r = rand(1,2);
+        $r = rand(1,4);
         switch ($r) {
             case 1:
                 Dazz::SellMicro();
@@ -31,6 +23,19 @@ class Ticker extends Model
                 Gac::BuyMicro();
                 sleep(10);
                 break;
+            case 3:
+                Dash::SellMicro();
+                sleep(10);
+                Dash::BuyMicro();
+                sleep(10);
+                break;
+            case 4:
+                Cto::SellMicro();
+                sleep(10);
+                Cto::BuyMicro();
+                sleep(10);
+                break;
+
         }
         // Cto::CacheTicker();
     }
