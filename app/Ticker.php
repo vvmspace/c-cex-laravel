bcd3e2a52a91101118d822d1dc051be2ac7db442
Vladimir V. Myagdeev <vvmspace@gmail.com>
2017-10-23 20:44:15 +0500

    Updated Ticker

--- app/Ticker.php
+++ app/Ticker.php
@@ -13,27 +13,15 @@ class Ticker extends Model
 
     static function MicroTick(){
         $t = 8;
-        $r = rand(1,4);
+        $r = rand(1,2);
         switch ($r) {
             case 1:
-                Dazz::SellMicro();
-                sleep($t);
-                Dazz::BuyMicro();
-                sleep($t);
-                break;
-            case 2:
-                Gac::SellMicro();
-                sleep($t);
-                Gac::BuyMicro();
-                sleep($t);
-                break;
-            case 3:
                 Dash::SellMicro();
                 sleep($t);
                 Dash::BuyMicro();
                 sleep($t);
                 break;
-            case 4:
+            case 2:
                 Cto::SellMicro();
                 sleep($t);
                 Cto::BuyMicro();
