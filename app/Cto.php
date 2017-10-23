e81bb31ddbabf96c757c3407286bae9d760dad39
Vladimir V. Myagdeev <vvmspace@gmail.com>
2017-10-23 20:51:31 +0500

    Updated CTO size

--- app/Cto.php
+++ app/Cto.php
@@ -45,7 +45,7 @@ class Cto extends AbstractPair
     static function BuyMicro(){
 	   $cto = new Cto;
        $price = Cto::RandomBuyPrice();
-       $size = 0.0001;
+       $size = 0.0005;
        $cto->api->makeOrder('buy', 'cto-btc', $size/$price , $price);
     }
 
