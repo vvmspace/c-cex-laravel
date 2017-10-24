5c5f8f5f2ec44497c967d7395d3c9b280ce7abd9
Vladimir V. Myagdeev <vvmspace@gmail.com>
2017-10-24 21:18:16 +0500

    Updated Cto config

--- config/pairs.php
+++ config/pairs.php
@@ -4,9 +4,9 @@ return [
     'cto-btc' => [
         'buy' => [
             'enabled' => true,
-            'from' => 10,
-            'to' => 120,
-            'size' => 0.0005,
+            'from' => 50,
+            'to' => 140,
+            'size' => 0.0002,
             'random' => 'classic'
         ],
         'sell' => [
