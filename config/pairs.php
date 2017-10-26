daed8f793cdf4260475b99181aea1253f87272c4
Vladimir V. Myagdeev <vvmspace@gmail.com>
2017-10-26 19:55:35 +0500

    Updated configs

--- config/pairs.php
+++ config/pairs.php
@@ -4,7 +4,7 @@ return [
     'cto-btc' => [
         'class' => 'Cto',
         'buy' => [
-            'enabled' => true,
+            'enabled' => false,
             'from' => 10,
             'to' => 120,
             'size' => 0.00015,
@@ -39,15 +39,15 @@ return [
         'class' => 'Dash',
         'buy' => [
             'enabled' => true,
-            'from' => 0.048 * 100000000,
-            'to' => 0.0502 * 100000000,
+            'from' => 0.043 * 100000000,
+            'to' => 0.0477 * 100000000,
             'size' => 0.0003,
             'random' => 'classic'
         ],
         'sell' => [
             'enabled' => true,
-            'from' => 0.0508 * 100000000,
-            'to' => 0.055 * 100000000,
+            'from' => 0.0501 * 100000000,
+            'to' => 0.0502 * 100000000,
             'size' => 0.0003,
             'random' => 'classic'
         ]
