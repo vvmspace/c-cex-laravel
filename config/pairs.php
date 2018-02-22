b64f0065ef3f3bbf926d7869353c5d5495efd812
vvmsecured <iva9Yatoj>
2018-02-22 17:11:23 +0500

    CTO from 100

--- config/pairs.php
+++ config/pairs.php
@@ -5,7 +5,7 @@ return [
         'class' => 'Cto',
         'buy' => [
             'enabled' => true,
-            'from' => 175,
+            'from' => 100,
             'to' => 250,
             'size' => 0.00011,
             'random' => 'low'
