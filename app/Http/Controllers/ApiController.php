50ec0d859825c8f8afbb68aa2518332beb6ecd75
vvmsecured <vvmspace@gmail.com>
2018-06-05 12:27:36 +0500

    /api/sin/cto-btc

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Cto;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    function sinCto(){
+        $cto = new Cto();
+        return ['to' => $cto->to()];
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('/works', function (Request $request){
     return 'works';
 });
+
+Route::get('/sin/cto-btc', 'ApiController@sinCto');
